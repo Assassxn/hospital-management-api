5dc94aaa03be0e3d194c113d7cc51d282ea25008
Assassxn <adnanalepo2005@gmail.com>
2025-04-14 23:28:57 +0100

    finalised all endpoints and their testing

--- src/main/java/co2124/part1/controller/MedicalRecordRestController.java
+++ src/main/java/co2124/part1/controller/MedicalRecordRestController.java
@@ -3,10 +3,12 @@ package co2124.part1.controller;
 import co2124.part1.domain.MedicalRecordCreationDTO;
 import co2124.part1.entities.Appointment;
 import co2124.part1.entities.MedicalRecord;
+import co2124.part1.exception.CustomValidationErrorResponse;
 import co2124.part1.respos.AppointmentRepository;
 import co2124.part1.respos.MedicalRecordRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/medical-records")
@@ -26,8 +29,18 @@ public class MedicalRecordRestController {
     private AppointmentRepository appointmentRepository;
 
     @PostMapping
-    public ResponseEntity<MedicalRecord> addMedicalRecord(@Valid @RequestBody MedicalRecordCreationDTO medicalRecord) {
+    public ResponseEntity<?> addMedicalRecord(@Valid @RequestBody MedicalRecordCreationDTO medicalRecord) {
         Appointment appointment = appointmentRepository.findAppointmentById(medicalRecord.getAppointmentId());
+        if (appointment.getMedicalRecord() != null) {
+            CustomValidationErrorResponse error = new CustomValidationErrorResponse(
+                    LocalDateTime.now(),
+                    HttpStatus.BAD_REQUEST.value(),
+                    HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                    List.of("Medical record already exists for appointment ID: " + medicalRecord.getAppointmentId())
+            );
+
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
 
         MedicalRecord newMedicalRecord = new MedicalRecord();
         newMedicalRecord.setDiagnosis(medicalRecord.getDiagnosis());
